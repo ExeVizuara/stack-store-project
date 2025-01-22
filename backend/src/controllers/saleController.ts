b868360899ecb5414d99ccee1c91e29755408b76
ExeVizuara <vizuara01@gmail.com>
2025-01-22 00:28:30 -0300

    feat: added getSalesOfTheDay function and modified sale data

--- backend/src/controllers/saleController.ts
+++ backend/src/controllers/saleController.ts
@@ -1,19 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import { SaleService } from '../services/saleService';
+import { Sale } from '../models';
+import { Op } from 'sequelize';
+import Product from '../models/Product';
 
 export const createSale = async (req: Request, res: Response, next: NextFunction) => {
   
-  const { user_id, product_id, product_name, product_category, product_quantity, product_price } = req.body;
+  const { user_id, products } = req.body;
   
   try {
-    const user = await SaleService.createSale({
-      user_id,
-      product_id,
-      product_name,
-      product_category,
-      product_quantity,
-      product_price
-    });
+    const user = await SaleService.createSale({ user_id, products });
     res.status(201).json({ message: 'Venta registrada con éxito' });
   } catch (error) {
     next(error instanceof Error ? error : new Error('Error desconocido en createSale'));
@@ -27,6 +23,32 @@ export const getAllSales = async (req: Request, res: Response, next: NextFunctio
     res.status(200).json(sales);
   } catch (error) {
     next(error instanceof Error ? error : new Error('Error desconocido en getAllSales'));
+  }
+};
+
+export const getSalesOfTheDay = async (req: Request, res: Response) => {
+  try {
+    const today = new Date();
+    today.setHours(0, 0, 0, 0); // Establece la hora a 00:00:00 para comparar solo la fecha
+
+    const filteredSales = await Sale.findAll({
+      where: {
+        createdAt: {
+          [Op.gte]: today // Filtra ventas creadas desde el inicio del día
+        }
+      },
+      include: [
+        {
+          model: Product,
+          through: { attributes: ['quantity'] } // Incluye los productos y sus cantidades
+        }
+      ]
+    });
+
+    res.status(200).json(filteredSales);
+  } catch (error) {
+    console.error('Error al obtener las ventas del día:', error);
+    res.status(500).json({ error: 'Error al obtener las ventas del día' });
   }
 };
 

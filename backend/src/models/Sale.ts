e1653319bf6703209469278f2f8d4ae6c9dbaafc
ExeVizuara <vizuara01@gmail.com>
2025-01-22 00:21:49 -0300

    feat: modified interface data to add relationship between tables

--- backend/src/models/Sale.ts
+++ backend/src/models/Sale.ts
@@ -1,71 +1,43 @@
 import { DataTypes, Model, Optional } from "sequelize";
 import sequelize from '../config/database';
 
-interface SaleAttributes {
+export interface SaleAttributes {
     id: number;
     user_id: number;
-    product_id: number;
-    product_name: string;
-    product_category: string;
-    product_quantity: number;
-    product_price: number;
-    created_at?: Date;
+    total: number;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
 
-type SaleCretionAttributes = Optional<SaleAttributes, 'id' | 'created_at'>;
+type SaleCreationAttributes = Optional<SaleAttributes, 'id'>;
 
-class Sale extends Model<SaleAttributes, SaleCretionAttributes> implements SaleAttributes {
+class Sale extends Model<SaleAttributes, SaleCreationAttributes> implements SaleAttributes {
     public id!: number;
     public user_id!: number;
-    public product_id!: number;
-    public product_name!: string;
-    public product_category!: string;
-    public product_quantity!: number;
-    public product_price!: number;
-    public readonly created_at!: Date;
+    public total!: number;
+    public readonly createdAt!: Date;
+    public readonly updatedAt!: Date;
 }
 
-Sale.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        product_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        product_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        product_category: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        product_quantity: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        product_price: {
-            type: DataTypes.FLOAT,
-            allowNull: false,
-        },
-        created_at: {
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW,
-        },
-    }, {
+Sale.init({
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    total: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    }
+  }, {
     sequelize,
     modelName: 'Sale',
     tableName: 'sales',
-    timestamps: false,
-}
-);
-
-export default Sale;
+    timestamps: true  // Esto habilita createdAt y updatedAt automáticamente
+  });
+  
+  export default Sale;

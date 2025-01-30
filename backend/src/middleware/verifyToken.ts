0ad9dd063a0f155879ad976a29c5a39213c69f9a
ExeVizuara <vizuara01@gmail.com>
2025-01-30 10:45:48 -0300

    feat: update config JWT

--- backend/src/middleware/verifyToken.ts
+++ backend/src/middleware/verifyToken.ts
@@ -1,24 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 export const verifyToken = (req: Request, res: Response, next: NextFunction): void => {
-    console.log('Cookies:', req.cookies);
     const token = req.cookies?.token;
+    console.log('Token recibido:', token); // Log para verificar que el token esté allí
     if (!token) {
-        res.status(401).json({ error: 'Acceso no autorizado' });
-        return;
+      res.status(401).json({ error: 'Acceso no autorizado' });
+      return;
     }
+  
     try {
-        const decoded = jwt.verify(token, process.env.JWT_TOKEN || 'test');
-        req.user = decoded;
-        next();
-    } catch (error: unknown) {
-        if (error instanceof jwt.TokenExpiredError) {
-            res.status(401).json({ error: 'Token expirado. Por favor, inicia sesión nuevamente.' });
-            return;
-        }
-        res.status(401).json({ error: 'Token inválido o expirado' });
-        return;
+      const decoded = jwt.verify(token, process.env.JWT_TOKEN || 'test') as JwtPayload;
+      req.user = { id: decoded.id as string };
+      next();
+    } catch (error) {
+      res.status(401).json({ error: 'Token inválido o expirado' });
+      return;
     }
-};
+  };
